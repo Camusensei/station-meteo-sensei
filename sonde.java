35cc5e5f44e4a9c49078aa35ff3b394badc07b2e
lloydsensei <lloydsensei@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-11 14:21:24 +0000

    ajout des fichiers

---
+++ sonde.java
@@ -0,0 +1,14 @@
+package Meteo;
+
+import java.util.Observable;
+
+
+public abstract class sonde extends Observable implements Runnable {
+
+		
+		/**
+		 */
+		public void run(){
+		}
+
+}
---
+++ unit.java
@@ -0,0 +1,35 @@
+package Meteo;
+
+
+public class unit {
+
+	/**
+	 * @uml.property  name="Precision"
+	 */
+	public int precision = 0;
+
+		
+		/**
+		 */
+		public String getFormattedValue(float valeur){
+			return "";	
+		}
+
+
+			
+			/**
+			 */
+			public String getSymbol(){
+				return "";	
+			}
+
+
+
+				
+				/**
+				 */
+				public String getDescription(){
+					return "";	
+				}
+
+}
