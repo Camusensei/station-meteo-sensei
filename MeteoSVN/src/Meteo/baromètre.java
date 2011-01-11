f9708af19799d32df5f5e4515a556b2324e08216
lloydsensei <lloydsensei@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-11 17:04:45 +0000

    indent

--- MeteoSVN/src/Meteo/baromètre.java
+++ MeteoSVN/src/Meteo/baromètre.java
@@ -4,8 +4,8 @@ import java.util.Observable;
 
 public class baromètre extends Capteur_pmt {
 
-	public baromètre(Observable obs,String name) {
-		super(obs,name);
+	public baromètre(Observable obs, String name) {
+		super(obs, name);
 	}
 
 }
--- MeteoSVN/src/Meteo/sonde_random.java
+++ MeteoSVN/src/Meteo/sonde_random.java
@@ -1,13 +1,8 @@
 package Meteo;
 
-
 public class sonde_random extends sonde {
 
-				
-				
-					
-					
-					public sonde_random(float min, float max){
-					}
+	public sonde_random(float min, float max) {
+	}
 
 }
