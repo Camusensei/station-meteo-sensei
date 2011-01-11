376d7a628c8cdb47e4d15ad707715b5db7152a9c
lloydsensei <lloydsensei@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-11 14:21:24 +0000

    ajout des fichiers

---
+++ trunk/Capteur_pmt.java
@@ -0,0 +1,60 @@
+package Meteo;
+
+import java.util.Observable;
+
+public abstract class Capteur_pmt extends Capteur_pm implements I_capteur_trend {
+
+	private float[] last5;
+	private int where = 0;
+	private boolean init = true;
+
+	public Capteur_pmt(Observable obs) {
+		super(obs);
+		where = 0;
+		init = true;
+		for (int i = 0; i < 5; i++) {
+			last5[i] = 0;
+		}
+	}
+
+	public float get_trend() {
+		float trend = 0;
+		int poids = 0;
+		if (init == true && where == 0) {
+			return 0;
+		}
+		if (init == true) {
+			for (int i = 1; i < where; i++) {
+				trend = (trend * poids + (last5[i] - last5[i - 1]))
+						/ (poids + 1);
+				poids++;
+			}
+		} else {
+			for (int i = 1; i < 5; i++) {
+				trend = (trend * poids + (last5[i] - last5[i - 1]))
+						/ (poids + 1);
+				poids++;
+			}
+		}
+		return trend;
+	}
+
+	@Override
+	public void clear_trend() {
+		int i;
+		for (i = 0; i < 5; i++)
+			last5[i] = 0;
+		init = true;
+		where = 0;
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+			 */
+	@Override
+	public void update(Observable obs, Object arg1) {
+		super.update(obs, arg1);
+	}
+
+}
