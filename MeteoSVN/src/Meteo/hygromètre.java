f4eabc77f292ea36c0b635642a1aef8a7eb493b8
lloydsensei <lloydsensei@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-17 05:34:51 +0000

    corrected all constructors x3

--- MeteoSVN/src/Meteo/hygromètre.java
+++ MeteoSVN/src/Meteo/hygromètre.java
@@ -6,8 +6,7 @@ import java.util.Observable;
 public class hygromètre extends Capteur_pm {
 
 	public hygromètre(List<unit> unités, String name, Observable observable,
-			float mem_max, float mem_min, float plage_min, float plage_max)
-			throws Exception {
+			float plage_min, float plage_max) throws Exception {
 		super(unités, name, observable, plage_min, plage_max);
 		// TODO Auto-generated constructor stub
 	}
