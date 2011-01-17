5af0ae083b0378ffa10399aaa9417335ce963c42
corentin.deluce <corentin.deluce@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-17 04:16:47 +0000

    ajout de la console

--- MeteoSVN/src/Meteo/console.java
+++ MeteoSVN/src/Meteo/console.java
@@ -9,13 +9,17 @@ public class console implements Runnable {
 	@Override
 	public synchronized void run() {
 		while(true){
+			System.out.println(Capteur_base.capteurs);
 				try {
 					val=System.in.read();
 				} catch (IOException e) {
 					e.printStackTrace();
 					break;
 				}
-				System.out.println(val);
+				System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+				if (val==113){
+					System.exit(0);
+				}
 						
 			Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 		}
