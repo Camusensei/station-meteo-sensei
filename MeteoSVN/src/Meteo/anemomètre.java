e09fc478bdcc1cd5d63dae5bd87544a0c4e22c75
corentin.deluce <corentin.deluce@224a0d5d-c88b-31bd-5674-2430c6eebe93>
2011-01-18 22:49:05 +0000

    comments

--- MeteoSVN/src/Meteo/anemomètre.java
+++ MeteoSVN/src/Meteo/anemomètre.java
@@ -2,8 +2,19 @@ package Meteo;
 
 import java.util.Observable;
 
+/**
+ * La classe anemomètre.
+ */
 public class anemomètre extends Capteur_pm {
 
+	/**
+	 * Instantiates a new anemomètre.
+	 *
+	 * @param observable l'observable associé
+	 * @param plage_min la plage minimale de fonctionnement
+	 * @param plage_max la plage maximale de fonctionnement
+	 * @throws Exception l'éventuelle exception
+	 */
 	public anemomètre(Observable observable, float plage_min, float plage_max)
 			throws Exception {
 		super(Preparation.unités_vitesses, "Anémomètre", observable, plage_min,
